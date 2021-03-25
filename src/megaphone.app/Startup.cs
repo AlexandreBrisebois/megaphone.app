64b7d5eabe036ee9bde4245535560723668faaea
Alexandre Brisebois <brisebois@outlook.com>
2021-03-24 23:11:30 -0400

    update

--- src/megaphone.app/Startup.cs
+++ src/megaphone.app/Startup.cs
@@ -24,7 +24,7 @@ namespace Megaphone.App
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddDapr();
 
             services.AddRazorPages();
             services.AddServerSideBlazor();
